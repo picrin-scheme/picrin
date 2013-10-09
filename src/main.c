4cdd8e8b018031f715654d8b1922378a0a65149d
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2013-10-09 17:10:32 +0900

    add simple repl (just echoing the input)

--- src/main.c
+++ src/main.c
@@ -3,6 +3,28 @@
 int
 main()
 {
-  puts("hello");
+  char line[256], last_char;
+  int char_index;
+
+  while (1) {
+    printf("> ");
+
+    char_index = 0;
+    while ((last_char = getchar()) != '\n') {
+      if (last_char == EOF)
+	goto eof;
+      line[char_index++] = last_char;
+    }
+    line[char_index] = '\0';
+
+    /* echo */
+    printf("%s", line);
+
+    printf("\n");
+  }
+
+ eof:
+  puts("");
+
   return 0;
 }
