fc9e2401a323a908552a0d321a99d143c1a263bc
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2013-10-17 13:55:00 +0900

    add some comments on picconf.h

--- include/picconf.h
+++ include/picconf.h
@@ -1,13 +1,16 @@
 #ifndef PICCONF_H__
 #define PICCONF_H__
 
+/* switch normal VM and direct threaded VM */
 #define PIC_DIRECT_THREADED_VM 1
 
+/* initial memory size (to be dynamically extended if necessary) */
 #define PIC_ARENA_SIZE 100
 #define PIC_HEAP_SIZE 8192
 #define PIC_STACK_SIZE 1024
 #define PIC_IREP_SIZE 256
 
+/* enable all debug flags */
 #define DEBUG 1
 
 #if DEBUG
