510b84702ed6b0f4a6b4b7c7fa4c21e2f3b25061
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2013-10-20 14:07:27 +0900

    eq? compares symbol's ptr

--- src/bool.c
+++ src/bool.c
@@ -12,7 +12,7 @@ pic_eq_p(pic_state *pic, pic_value x, pic_value y)
   case PIC_TT_NIL:
     return true;
   case PIC_TT_SYMBOL:
-    return strcmp(pic_symbol_ptr(x)->name, pic_symbol_ptr(y)->name) == 0;
+    return pic_symbol_ptr(x) == pic_symbol_ptr(y);
   default:
     return false;
   }
