808fe32ac03fa7367532ffbd4c2c93327c529084
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2013-11-03 11:39:13 -0500

    add boolean functions

--- src/bool.c
+++ src/bool.c
@@ -28,8 +28,32 @@ pic_bool_eq_p(pic_state *pic)
   return pic_bool_value(pic_eq_p(pic, x, y));
 }
 
+/* TODO: replace it with native opcode */
+static pic_value
+pic_bool_not(pic_state *pic)
+{
+  pic_value v;
+
+  pic_get_args(pic, "o", &v);
+
+  return pic_false_p(v) ? pic_true_value() : pic_false_value();
+}
+
+static pic_value
+pic_bool_boolean_p(pic_state *pic)
+{
+  pic_value v;
+
+  pic_get_args(pic, "o", &v);
+
+  return (pic_true_p(v) || pic_false_p(v)) ? pic_true_value() : pic_false_value();
+}
+
 void
 pic_init_bool(pic_state *pic)
 {
   pic_defun(pic, "eq?", pic_bool_eq_p);
+
+  pic_defun(pic, "not", pic_bool_not);
+  pic_defun(pic, "boolean?", pic_bool_boolean_p);
 }
