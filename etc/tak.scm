70fbe46dc17c0fa8377d5fec663b53c0ce0e0cdb
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2013-11-04 13:19:04 -0500

    cleanup etc/tak.scm

--- etc/tak.scm
+++ etc/tak.scm
@@ -1,16 +1,12 @@
-(define start #f)
-
-(define end #f)
-
 (define (time f)
-  (set! start (current-jiffy))
-  (f)
-  (set! end (current-jiffy))
-  (/ (- end start) (jiffies-per-second)))
+  (let ((start (current-jiffy)))
+    (f)
+    (/ (- (current-jiffy) start)
+       (jiffies-per-second))))
 
 (define (tak x y z)
   (if (> x y)
-      (tak (tak (- x 1) y z) 
+      (tak (tak (- x 1) y z)
 	   (tak (- y 1) z x)
 	   (tak (- z 1) x y))
       y))
@@ -19,3 +15,4 @@
   (tak 12 6 0))
 
 (write (time f))
+(newline)
