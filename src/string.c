73d9b0dc79c2c191a10fbb1a1d16a27b609cf13c
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2013-11-15 17:08:18 +0900

    use strndup if possible

--- src/string.c
+++ src/string.c
@@ -6,15 +6,10 @@ pic_value
 pic_str_new(pic_state *pic, const char *cstr, size_t len)
 {
   struct pic_string *str;
-  char *new_str;
-
-  new_str = (char *)pic_alloc(pic, len + 1);
-  strncpy(new_str, cstr, len);
-  new_str[len] = '\0';
 
   str = (struct pic_string *)pic_obj_alloc(pic, sizeof(struct pic_string), PIC_TT_STRING);
   str->len = len;
-  str->str = new_str;
+  str->str = strdup(cstr);
 
   return pic_obj_value(str);
 }
