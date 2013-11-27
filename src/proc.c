c2d7f3d1f2302165fc69eecf06b75483690326b6
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2013-11-27 06:31:49 -0800

    [bugfix] wrong size for pic_proc allocation

--- src/proc.c
+++ src/proc.c
@@ -7,7 +7,7 @@ pic_proc_new(pic_state *pic, struct pic_irep *irep, struct pic_env *env)
 {
   struct pic_proc *proc;
 
-  proc = (struct pic_proc *)pic_obj_alloc(pic, sizeof(struct pic_proc *), PIC_TT_PROC);
+  proc = (struct pic_proc *)pic_obj_alloc(pic, sizeof(struct pic_proc), PIC_TT_PROC);
   proc->cfunc_p = false;
   proc->u.irep = irep;
   proc->env = env;
@@ -19,7 +19,7 @@ pic_proc_new_cfunc(pic_state *pic, pic_func_t cfunc)
 {
   struct pic_proc *proc;
 
-  proc = (struct pic_proc *)pic_obj_alloc(pic, sizeof(struct pic_proc *), PIC_TT_PROC);
+  proc = (struct pic_proc *)pic_obj_alloc(pic, sizeof(struct pic_proc), PIC_TT_PROC);
   proc->cfunc_p = true;
   proc->u.cfunc = cfunc;
   proc->env = NULL;
