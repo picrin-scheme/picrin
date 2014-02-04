d3a3c788292963d50d226dd9bc70beff0934afa5
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-02-04 14:35:42 +0900

    add tail-call test

---
+++ t/tail-call.scm
@@ -0,0 +1,13 @@
+(import (scheme base))
+
+;;; always returns zero
+(define (zero n)
+  (if (zero? n)
+      0
+      (zero (- n 1))))
+
+;;; using apply
+(define (zero-2 n)
+  (if (zero? n)
+      0
+      (apply zero-2 (list (- n 1)))))
