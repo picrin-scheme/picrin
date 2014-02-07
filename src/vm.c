1afbfcda235bbf5f5f9e27a07eec3b9eccff9928
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-02-07 10:02:09 +0900

    add 'static'

--- src/vm.c
+++ src/vm.c
@@ -815,7 +815,7 @@ pic_apply(pic_state *pic, struct pic_proc *proc, pic_value argv)
   } VM_LOOP_END;
 }
 
-struct pic_code trampoline_iseq[] = {
+static struct pic_code trampoline_iseq[] = {
   { OP_NOP,		{0} },
   { OP_TAILCALL,	{0} },
 };
