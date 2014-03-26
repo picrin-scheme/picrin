a3428588f7794bab89496cc193f751b4e1dc9a8d
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-03-26 16:46:07 +0900

    add srfi-111

---
+++ piclib/srfi/111.scm
@@ -0,0 +1,10 @@
+(define-library (srfi 111)
+  (import (scheme base))
+
+  (define-record-type box
+    (box value)
+    box?
+    (value unbox set-box!))
+
+  (export box box?
+          unbox set-box!))
