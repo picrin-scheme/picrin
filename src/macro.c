45496251822255e43adcd8cef3ebd9405e02eb7d
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-03-31 08:27:04 +0900

    cleanup

--- src/macro.c
+++ src/macro.c
@@ -509,8 +509,7 @@ macroexpand_node(pic_state *pic, pic_value expr, struct pic_senv *senv)
   case PIC_TT_VAR:
   case PIC_TT_IREP:
   case PIC_TT_DATA:
-    pic_error(pic, "unexpected value type");
-    return pic_undef_value();	/* unreachable */
+    pic_errorf(pic, "unexpected value type: ~s", expr);
   }
   UNREACHABLE();
 }
