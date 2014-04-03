accfd2ee75d9e132fe4386ff007522175afa7d0a
koba-e964 <koba-e964@debian-koba-e964-0.(none)>
2014-04-03 22:59:18 +0900

    Fix #99
    Changes pic->ci after errors

--- tools/main.c
+++ tools/main.c
@@ -127,6 +127,7 @@ repl(pic_state *pic)
     pic_catch {
       printf("%s\n", pic_str_cstr(pic_get_backtrace(pic)));
       pic->err = NULL;
+      pic->ci = pic->cibase;
       code[0] = '\0';
     }
 
