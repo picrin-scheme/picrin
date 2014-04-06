920524dc40301469f30952f5de21b4be51de713f
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-04-06 14:00:45 +0900

    add pic_print_backtrace function

--- src/debug.c
+++ src/debug.c
@@ -14,11 +14,38 @@ pic_get_backtrace(pic_state *pic)
   pic_callinfo *ci;
   pic_str *trace;
 
+  trace = pic_str_new(pic, NULL, 0);
+
+  for (ci = pic->ci; ci != pic->cibase; --ci) {
+    struct pic_proc *proc = pic_proc_ptr(ci->fp[0]);
+
+    trace = pic_strcat(pic, trace, pic_str_new_cstr(pic, "  at "));
+    trace = pic_strcat(pic, trace, pic_str_new_cstr(pic, pic_symbol_name(pic, pic_proc_name(proc))));
+
+    if (pic_proc_func_p(proc)) {
+      trace = pic_strcat(pic, trace, pic_str_new_cstr(pic, " (native function)\n"));
+    } else if (pic_proc_irep_p(proc)) {
+      trace = pic_strcat(pic, trace, pic_str_new_cstr(pic, " (unknown location)\n")); /* TODO */
+    }
+  }
+
+  pic_gc_arena_restore(pic, ai);
+  pic_gc_protect(pic, pic_obj_value(trace));
+
+  return trace;
+}
+
+void
+pic_print_backtrace(pic_state *pic, struct pic_error *e)
+{
+  int ai = pic_gc_arena_preserve(pic);
+  pic_str *trace;
+
   assert(pic->err != NULL);
 
   trace = pic_str_new(pic, NULL, 0);
 
-  switch (pic->err->type) {
+  switch (e->type) {
   case PIC_ERROR_OTHER:
     trace = pic_strcat(pic, trace, pic_str_new_cstr(pic, "error: "));
     break;
@@ -33,25 +60,15 @@ pic_get_backtrace(pic_state *pic)
     break;
   }
 
-  trace = pic_strcat(pic, trace, pic->err->msg);
+  trace = pic_strcat(pic, trace, e->msg);
 
   /* TODO: print error irritants */
 
-  for (ci = pic->ci; ci != pic->cibase; --ci) {
-    struct pic_proc *proc = pic_proc_ptr(ci->fp[0]);
+  trace = pic_strcat(pic, trace, pic_str_new(pic, "\n", 1));
+  trace = pic_strcat(pic, trace, pic_get_backtrace(pic));
 
-    trace = pic_strcat(pic, trace, pic_str_new_cstr(pic, "\n  at "));
-    trace = pic_strcat(pic, trace, pic_str_new_cstr(pic, pic_symbol_name(pic, pic_proc_name(proc))));
-
-    if (pic_proc_func_p(proc)) {
-      trace = pic_strcat(pic, trace, pic_str_new_cstr(pic, " (native function)"));
-    } else if (pic_proc_irep_p(proc)) {
-      trace = pic_strcat(pic, trace, pic_str_new_cstr(pic, " (unknown location)")); /* TODO */
-    }
-  }
+  /* print! */
+  printf("%s", pic_str_cstr(trace));
 
   pic_gc_arena_restore(pic, ai);
-  pic_gc_protect(pic, pic_obj_value(trace));
-
-  return trace;
 }
