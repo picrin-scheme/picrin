44b80ccb8a46ef37e6c8458b3167abc77c177781
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-06-22 20:24:29 +0900

    remove file-variable

--- t/r7rs-tests.scm
+++ t/r7rs-tests.scm
@@ -25,9 +25,6 @@
 ;; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 ;; THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
-;; -*- coding: utf-8 -*-
-
 (import (scheme base)
 ;        (scheme char)
         (scheme lazy)
