bd75ab3c49242cb1f6c938c84558311f4f735892
Sunrim KIM (keen) <3han5chou7@gmail.com>
2014-07-05 15:14:08 +0900

    do with :TODO:s

--- include/picrin/number.h
+++ include/picrin/number.h
@@ -10,6 +10,7 @@ extern "C" {
 #endif
 
 #include <gmp.h>
+#include <mpfr.h>
 
 #define pic_bigint_p(v) (pic_type(v) == PIC_TT_BIGINT)
 #define pic_bigint_ptr(o) ((struct pic_bigint *)pic_ptr(o))
@@ -33,9 +34,9 @@ pic_bigfloat *pic_bigfloat_new(pic_state *);
 #define pic_number_to_int(o)                                            \
     (pic_int_p(o)      ? pic_int(o)                             :       \
      pic_float_p(o)    ? (int)pic_float(o)                      :       \
-     pic_bigint_p(o)   ? (int)mpz_get_si(pic_bigint_ptr(o)->z)  :       \
+     pic_bigint_p(o)   ?      mpz_get_si(pic_bigint_ptr(o)->z)  :       \
      pic_rational_p(o) ? (int)mpq_get_d(pic_rational_ptr(o)->q) :       \
-     (int) mpfr_get_d(pic_bigfloat_ptr))
+     mpfr_get_si(pic_bigfloat_ptr))
     
     
 #define pic_number_to_float(o)                                  \
@@ -63,16 +64,34 @@ pic_number_normalize(pic_state *pic, pic_value *v)
       }
     }
     if(pic_bigint_p(*v)){
-      pic_bigint *bi  = pic_bigint_ptr(*v);
-      if(mpz_fits_sint_p(bi->z)){
+      pic_bigint *z  = pic_bigint_ptr(*v);
+      if(mpz_fits_sint_p(z->z)){
         pic_init_value(*v, PIC_VTYPE_INT);
-        v->u.i = mpz_get_si(bi->z);
-        mpz_clear(bi->z);
-        pic_free(pic, bi);
+        v->u.i = mpz_get_si(z->z);
+        /* mpz_clear(z->z); */
+        /* pic_free(pic, z); */
       }
     }
   }
 }
+
+#define mpfr_si_add(rop, op1, op2, rnd) mpfr_add_si(rop, op2, op1, rnd)
+#define mpfr_d_add(rop, op1, op2, rnd) mpfr_add_d(rop, op2, op1, rnd)
+#define mpfr_z_add(rop, op1, op2, rnd) mpfr_add_z(rop, op2, op1, rnd)
+#define mpfr_q_add(rop, op1, op2, rnd) mpfr_add_q(rop, op2, op1, rnd)
+
+#define mpfr_q_sub(rop, op1, op2, rnd) mpfr_sub_q(rop, op2, op1, rnd);mpfr_neg(rop, rop, rnd);
+
+#define mpfr_si_mul(rop, op1, op2, rnd) mpfr_mul_si(rop, op2, op1, rnd)
+#define mpfr_d_mul(rop, op1, op2, rnd) mpfr_mul_d(rop, op2, op1, rnd)
+#define mpfr_z_mul(rop, op1, op2, rnd) mpfr_mul_z(rop, op2, op1, rnd)
+#define mpfr_q_mul(rop, op1, op2, rnd) mpfr_mul_q(rop, op2, op1, rnd)
+
+#define mpfr_z_div(rop, op1, op2, rnd) mpfr_div_z(rop, op2, op1, rnd);mpfr_ui_div(rop, 1, rop, rnd);
+#define mpfr_q_div(rop, op1, op2, rnd) mpfr_div_q(rop, op2, op1, rnd);mpfr_ui_div(rop, 1, rop, rnd);
+
+
+
 pic_bigint *pic_bigint_add(pic_state *, pic_bigint *, pic_bigint *);
 pic_bigint *pic_bigint_sub(pic_state *, pic_bigint *, pic_bigint *);
 pic_bigint *pic_bigint_mul(pic_state *, pic_bigint *, pic_bigint *);
