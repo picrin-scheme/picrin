73d63bafb2f993dca7326ab4baa35f8401101289
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-07-10 10:16:33 +0900

    uncomment doubly commented section headers (t/r7rs)

--- t/r7rs-tests.scm
+++ t/r7rs-tests.scm
@@ -2120,19 +2120,19 @@
 (test-numeric-syntax "#i1" 1.0 "1.0" "1.")
 (test-numeric-syntax "#I1" 1.0 "1.0" "1.")
 (test-numeric-syntax "#i-1" -1.0 "-1.0" "-1.")
-;; ;; Decimal
+;; Decimal
 (test-numeric-syntax "1.0" 1.0 "1.0" "1.")
 (test-numeric-syntax "1." 1.0 "1.0" "1.")
 ;; (test-numeric-syntax ".1" 0.1 "0.1" "100.0e-3")
 ;; (test-numeric-syntax "-.1" -0.1 "-0.1" "-100.0e-3")
-;; ;; Some Schemes don't allow negative zero. This is okay with the standard
+;; Some Schemes don't allow negative zero. This is okay with the standard
 (test-numeric-syntax "-.0" -0.0 "-0." "-0.0" "0.0" "0." ".0")
 (test-numeric-syntax "-0." -0.0 "-.0" "-0.0" "0.0" "0." ".0")
 (test-numeric-syntax "#i1.0" 1.0 "1.0" "1.")
 (test-numeric-syntax "#e1.0" 1 "1")
 (test-numeric-syntax "#e-.0" 0 "0")
 (test-numeric-syntax "#e-0." 0 "0")
-;; ;; Decimal notation with suffix
+;; Decimal notation with suffix
 (test-numeric-syntax "1e2" 100.0 "100.0" "100.")
 (test-numeric-syntax "1E2" 100.0 "100.0" "100.")
 ;; (test-numeric-syntax "1s2" 100.0 "100.0" "100.")
@@ -2143,7 +2143,7 @@
 ;; (test-numeric-syntax "1D2" 100.0 "100.0" "100.")
 ;; (test-numeric-syntax "1l2" 100.0 "100.0" "100.")
 ;; (test-numeric-syntax "1L2" 100.0 "100.0" "100.")
-;; ;; NaN, Inf
+;; NaN, Inf
 (test-numeric-syntax "+nan.0" +nan.0 "+nan.0" "+NaN.0")
 (test-numeric-syntax "+NAN.0" +nan.0 "+nan.0" "+NaN.0")
 (test-numeric-syntax "+inf.0" +inf.0 "+inf.0" "+Inf.0")
@@ -2153,7 +2153,7 @@
 (test-numeric-syntax "#i+nan.0" +nan.0 "+nan.0" "+NaN.0")
 (test-numeric-syntax "#i+inf.0" +inf.0 "+inf.0" "+Inf.0")
 (test-numeric-syntax "#i-inf.0" -inf.0 "-inf.0" "-Inf.0")
-;; ;; Exact ratios
+;; Exact ratios
 (test-numeric-syntax "1/2" (/ 1 2))
 (test-numeric-syntax "#e1/2" (/ 1 2) "1/2")
 (test-numeric-syntax "10/2" 5 "5")
@@ -2161,7 +2161,7 @@
 (test-numeric-syntax "0/10" 0 "0")
 (test-numeric-syntax "#e0/10" 0 "0")
 (test-numeric-syntax "#i3/2" (/ 3.0 2.0) "1.5")
-;; ;; Exact complex
+;; Exact complex
 ;; (test-numeric-syntax "1+2i" (make-rectangular 1 2))
 ;; (test-numeric-syntax "1+2I" (make-rectangular 1 2) "1+2i")
 ;; (test-numeric-syntax "1-2i" (make-rectangular 1 -2))
@@ -2196,7 +2196,7 @@
 ;; ;; Complex numbers (polar notation)
 ;; ;; Need to account for imprecision in write output.
 ;; ;;(test-numeric-syntax "1@2" -0.416146836547142+0.909297426825682i "-0.416146836547142+0.909297426825682i")
-;; ;; Base prefixes
+;; Base prefixes
 (test-numeric-syntax "#x11" 17 "17")
 (test-numeric-syntax "#X11" 17 "17")
 (test-numeric-syntax "#d11" 11 "11")
@@ -2213,15 +2213,15 @@
 (test-numeric-syntax "#d-10" -10 "-10")
 (test-numeric-syntax "#o-10" -8 "-8")
 (test-numeric-syntax "#b-10" -2 "-2")
-;; ;; Combination of prefixes
+;; Combination of prefixes
 (test-numeric-syntax "#e#x10" 16 "16")
 (test-numeric-syntax "#i#x10" 16.0 "16.0" "16.")
-;; ;; (Attempted) decimal notation with base prefixes
+;; (Attempted) decimal notation with base prefixes
 (test-numeric-syntax "#d1." 1.0 "1.0" "1.")
 ;; (test-numeric-syntax "#d.1" 0.1 "0.1" ".1" "100.0e-3")
 (test-numeric-syntax "#x1e2" 482 "482")
 (test-numeric-syntax "#d1e2" 100.0 "100.0" "100.")
-;; ;; Fractions with prefixes
+;; Fractions with prefixes
 (test-numeric-syntax "#x10/2" 8 "8")
 (test-numeric-syntax "#x11/2" (/ 17 2) "17/2")
 (test-numeric-syntax "#d11/2" (/ 11 2) "11/2")
