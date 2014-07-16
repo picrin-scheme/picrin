8e114fae6b24b78c95c0d75e38695050f7d27b3c
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-07-16 14:32:04 +0900

    unlock letrec-syntax test

--- t/r7rs-tests.scm
+++ t/r7rs-tests.scm
@@ -464,24 +464,24 @@
     (let ((x 'inner))
       (m)))))
 
-;; (test 7 (letrec-syntax
-;;   ((my-or (syntax-rules ()
-;;             ((my-or) #f)
-;;             ((my-or e) e)
-;;             ((my-or e1 e2 ...)
-;;              (let ((temp e1))
-;;                (if temp
-;;                    temp
-;;                    (my-or e2 ...)))))))
-;;   (let ((x #f)
-;;         (y 7)
-;;         (temp 8)
-;;         (let odd?)
-;;         (if even?))
-;;     (my-or x
-;;            (let temp)
-;;            (if y)
-;;            y))))
+(test 7 (letrec-syntax
+  ((my-or (syntax-rules ()
+            ((my-or) #f)
+            ((my-or e) e)
+            ((my-or e1 e2 ...)
+             (let ((temp e1))
+               (if temp
+                   temp
+                   (my-or e2 ...)))))))
+  (let ((x #f)
+        (y 7)
+        (temp 8)
+        (let odd?)
+        (if even?))
+    (my-or x
+           (let temp)
+           (if y)
+           y))))
 
 (define-syntax be-like-begin
   (syntax-rules ()
