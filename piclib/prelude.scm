137a01e74e2244b5373f07fa30668712e6ef1cf1
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-07-27 14:29:29 +0900

    add environment procedure

--- piclib/prelude.scm
+++ piclib/prelude.scm
@@ -1087,6 +1087,19 @@
         (error "unsupported environment version" n)
         '(scheme r5rs)))
 
+  (define environment
+    (let ((counter 0))
+      (lambda specs
+        (let ((library-name `(picrin @@my-environment ,counter)))
+          (set! counter (+ counter 1))
+          (eval
+           `(define-library ,library-name
+              ,@(map (lambda (spec)
+                       `(import ,spec))
+                     specs))
+           '(scheme base))
+          library-name))))
+
   (export null-environment
           scheme-report-environment
-          ))
+          environment))
