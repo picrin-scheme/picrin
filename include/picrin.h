07b201c830a3f04a0fe1554b871fb6308ff35fcd
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-07-28 00:41:20 +0900

    undocument pic_defmacro

--- include/picrin.h
+++ include/picrin.h
@@ -145,7 +145,6 @@ pic_value pic_funcall(pic_state *pic, const char *name, pic_list args);
 struct pic_proc *pic_get_proc(pic_state *);
 int pic_get_args(pic_state *, const char *, ...);
 void pic_defun(pic_state *, const char *, pic_func_t);
-void pic_defmacro(pic_state *, const char *, struct pic_proc *);
 
 bool pic_equal_p(pic_state *, pic_value, pic_value);
 
