19470ec9de2312acd3a1187f6bfb9225a442e64b
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-08-09 17:29:52 +0900

    add (picrin control async)

---
+++ contrib/20.async/piclib/async.scm
@@ -0,0 +1,29 @@
+(define-library (picrin control async)
+  (import (scheme base)
+          (picrin control)
+          (picrin promise))
+
+ (define (promise-unit x)
+   (make-promise
+    (lambda (resolve _)
+      (resolve x))))
+
+ (define (promise-bind m f)
+   (promise-then m f))
+
+ (define-syntax async
+   (syntax-rules ()
+     ((_ x ...)
+      (reset (lambda ()
+               (promise-unit (begin x ...)))))))
+
+ (define (await m)
+   (shift (lambda (f)
+            (promise-bind m f))))
+
+ (define (force m)
+   (call/cc
+    (lambda (f)
+      (promise-bind m f))))
+
+ (export async await force))
