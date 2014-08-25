ff21555a76bfb99e52dd7c21298f4c5ed2b5318d
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-08-25 13:39:15 +0900

    remove needless files

--- include/.dir-locals.el
+++
@@ -1,3 +0,0 @@
-((c-mode . ((flycheck-clang-include-path . ( "../extlib"))
-            (flycheck-clang-warnings . ("all" "extra"))
-            (flycheck-clang-language-standard . "c99"))))
--- include/picrin/.dir-locals.el
+++
@@ -1,4 +0,0 @@
-((c-mode . ((flycheck-clang-includes . ("../picrin.h"))
-            (flycheck-clang-include-path . ( "../../extlib"))
-            (flycheck-clang-warnings . ("all" "extra"))
-            (flycheck-clang-language-standard . "c99"))))
