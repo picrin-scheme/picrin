ac15ac6e2d72b9290e3dae11bd50a190ac975790
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-08-31 01:00:13 +0900

    use readline in repl

--- contrib/20.repl/repl.scm
+++ contrib/20.repl/repl.scm
@@ -2,14 +2,16 @@
   (import (scheme base)
           (scheme read)
           (scheme write)
-          (scheme eval))
+          (scheme eval)
+          (picrin readline)
+          (picrin readline history))
 
   (define (repl)
-    (display "> ")
-    (let ((expr (read)))
-      (if (eof-object? expr)
+    (let ((line (readline "> ")))
+      (if (eof-object? line)
           (newline)                     ; exit
           (begin
+            (add-history line)
             (call/cc
              (lambda (exit)
               (with-exception-handler
@@ -18,8 +20,13 @@
                  (newline)
                  (exit))
                (lambda ()
-                 (write (eval expr '(picrin user)))
-                 (newline)))))
+                 (let ((port (open-input-string line)))
+                   (let loop ((expr (read port)))
+                     (unless (eof-object? expr)
+                       (write (eval expr '(picrin user)))
+                       (newline)
+                       (loop (read port))))
+                   (close-port port))))))
             (repl)))))
 
   (export repl))
