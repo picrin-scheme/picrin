6f40dcc5df4018f4ff2425937cddfe48637f80b2
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-09-04 19:14:43 +0900

    refactor define-record-writer

--- piclib/picrin/record.scm
+++ piclib/picrin/record.scm
@@ -1,17 +1,17 @@
 (define-library (picrin record)
   (import (scheme base))
 
-  (define (define-record-writer* record-type writer)
+  (define (set-record-writer! record-type writer)
     (record-set! record-type 'writer writer))
 
   (define-syntax define-record-writer
     (syntax-rules ()
       ((_ (type obj) body ...)
-       (define-record-writer* type
+       (set-record-writer! type
          (lambda (obj)
            body ...)))
       ((_ type writer)
-       (define-record-writer* type
+       (set-record-writer! type
          writer))))
 
   (export define-record-writer))
