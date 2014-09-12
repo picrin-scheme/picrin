642bd10fb3f86a717b380a52d29e23321cd9c260
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-09-12 19:54:21 +0900

    var_new -> make_var

--- include/picrin/var.h
+++ include/picrin/var.h
@@ -18,7 +18,7 @@ struct pic_var {
 #define pic_var_p(o) (pic_type(o) == PIC_TT_VAR)
 #define pic_var_ptr(o) ((struct pic_var *)pic_ptr(o))
 
-struct pic_var *pic_var_new(pic_state *, pic_value, struct pic_proc * /* = NULL */);
+struct pic_var *pic_make_var(pic_state *, pic_value, struct pic_proc * /* = NULL */);
 
 pic_value pic_var_ref(pic_state *, struct pic_var *);
 void pic_var_set(pic_state *, struct pic_var *, pic_value);
