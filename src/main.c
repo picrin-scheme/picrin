9eb938ff87ea8d3239f2af51fdcdf715c3095d93
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-09-15 16:42:57 +0900

    inline configuration

--- src/main.c
+++ src/main.c
@@ -2,7 +2,9 @@
  * See Copyright Notice in picrin.h
  */
 
-#include "config.h"
+#define PIC_ARENA_SIZE (8 * 1024)
+#define PIC_HEAP_PAGE_SIZE (2 * 1024 * 1024)
+#define PIC_SYM_POOL_SIZE (2 * 1024)
 
 #include "picrin.h"
 #include "picrin/pair.h"
