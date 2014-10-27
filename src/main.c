e132236445579fcf672abf2a97c222081cb15005
Sunrim KIM (keen) <3han5chou7@gmail.com>
2014-10-27 14:15:03 +0900

    restore tuned config

--- src/main.c
+++ src/main.c
@@ -2,10 +2,6 @@
  * See Copyright Notice in picrin.h
  */
 
-#define PIC_ARENA_SIZE (8 * 1024)
-#define PIC_HEAP_PAGE_SIZE (2 * 1024 * 1024)
-#define PIC_SYM_POOL_SIZE (2 * 1024)
-
 #include "picrin.h"
 #include "picrin/pair.h"
 #include "picrin/error.h"
