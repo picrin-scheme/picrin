54f01f6c2c36b69fd1d566432cd8031d49395892
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2015-01-18 13:01:16 +0000

    [WIP] implement simple-runner

--- piclib/picrin/test.scm
+++ piclib/picrin/test.scm
@@ -6,10 +6,28 @@
   (define-record-type <test-runner>
     (make-test-runner)
     test-runner?
+    ;; members
     (test-count test-count set-test-count!)
     (failure-count failure-count set-failure-count!)
     (failures failures set-failures!)
-    (suit-count suit-count set-suit-count!))
+    (suit-count suit-count set-suit-count!)
+
+    ;; call backs
+    (cb-begin cb-begin set-cb-begin!)
+    (cb-end cb-end set-cb-end!)
+    (cb-test-enter cb-test-enter set-cb-test-enter!)
+    (cb-test-pass cb-test-pass set-cb-test-pass!)
+    (cb-test-failure cb-test-failure set-cb-test-failure!)
+    (cb-test-error cb-test-error set-cb-test-error!)
+    (cb-test-exit cb-test-exit set-cb-test-exit!))
+
+  (define (null-cb-begin t name count) #f)
+  (define (null-cb-end t name) #f)
+  (define (null-cb-test-enter t) #f)
+  (define (null-cb-test-pass    t name expect form got) #f)
+  (define (null-cb-test-failure t name expect form got) #f)
+  (define (null-cb-test-error   t name expect form error) #f)
+  (define (null-cb-test-exit t) #f)
 
   (define (test-runner-null)
     (let ((t (make-test-runner)))
@@ -17,19 +35,21 @@
       (set-failure-count! t 0)
       (set-failures! t ())
       (set-suit-count! t 0)
+
+      (set-cb-test-enter! t null-cb-test-enter)
+      (set-cb-begin! t null-cb-begin)
+      (set-cb-end! t null-cb-end)
+      (set-cb-test-pass! t null-cb-test-pass)
+      (set-cb-test-exit! t null-cb-test-exit)
       t))
 
-  (define current-test-runner% (test-runner-null))
-
-
+  (define current-test-runner% #f)
   (define (current-test-runner . t)
     (if (null? t)
         current-test-runner%
         (set! current-test-runner% (car t))))
 
   (define no-name "no test name")
-
-  (define fails '())
 
   (define (print-statistics)
     (let ((t (current-test-runner)))
@@ -50,17 +70,7 @@
          (display fail))
        (reverse (failures t)))))
 
-  (define (test-begin . o)
-    (let ((t (current-test-runner)))
-      (set-suit-count! t (+ (suit-count t) 1))))
 
-  (define (test-end . o)
-    (let ((t (current-test-runner)))
-      (set-suit-count! t (- (suit-count t) 1))
-      (if (= (suit-count t) 0)
-          (print-statistics))))
-
-  
   (define (print-case out number test-name)
     (display "case " out)
     (write number out)
@@ -101,33 +111,88 @@
     (newline out)
     (display "[0;39m" out))
 
+  (define (simple-cb-begin t suit-name count)
+    (set-suit-count! t (+ (suit-count t) 1)))
+
+  (define (simple-cb-end t suit-name)
+    (set-suit-count! t (- (suit-count t) 1))
+    (if (= (suit-count t) 0)
+        (print-statistics)))
+
+  (define (call-with-handle-failure t test-name expected expr got proc)
+    (set-failure-count! t (+ (failure-count t) 1))
+    (let ((out (open-output-string))
+          (str #f))
+      (print-case out (test-count t) test-name)
+      (proc out test-name expected 'expr got)
+      (set! str (get-output-string out))
+      (set-failures! t (cons str (failures t)))
+      (display str)))
+
+
+  (define (simple-cb-test-pass t test-name expected expr got)
+    (print-case (current-output-port) (test-count t) test-name)
+    (print-pass (current-output-port) test-name expected expr))
+  
+  (define (simple-cb-test-failure t test-name expected expr got)
+    (call-with-handle-failure t test-name expected expr got
+                              print-fail))
+  (define (simple-cb-test-error t test-name expected expr got)
+    (call-with-handle-failure t test-name expected expr got
+                              print-error))
+  (define (simple-cb-test-exit t)
+    (set-test-count! t (+ (test-count t) 1)))
+
+
+  (define (test-runner-simple)
+    (let ((t (test-runner-null)))
+      (set-cb-begin! t simple-cb-begin)
+      (set-cb-end! t simple-cb-end)
+      
+      (set-cb-test-pass! t simple-cb-test-pass)
+      (set-cb-test-failure! t simple-cb-test-failure)
+      (set-cb-test-error! t simple-cb-test-error)
+      (set-cb-test-exit! t simple-cb-test-exit)
+      t))
+
+
+  (define-syntax test-begin
+    (syntax-rules ()
+      ((_ suit-name count)
+       (let ((t (current-test-runner)))
+         ((cb-begin t) t suit-name count)))
+      ((_ suit-name)
+       (test-begin suit-name 0))
+      ((_)
+       (test-begin #f))))
+
+
+  (define-syntax test-end
+    (syntax-rules ()
+      ((_ suit-name)
+       (let ((t (current-test-runner)))
+         ((cb-end t) t suit-name)))
+      ((_)
+       (test-end #f))))
+
   (define-syntax define-test-macro
     (syntax-rules ()
       ((_ test?) 
        (syntax-rules ()
          ((_ test-name expected expr)
-          (let* ((t (current-test-runner))
-                 (call-with-handle-failure (lambda (proc got)
-                                             (set-failure-count! t (+ (failure-count t) 1))
-                                             (let ((out (open-output-string))
-                                                   (str #f))
-                                               (print-case out (test-count t) test-name)
-                                               (proc out test-name expected 'expr got)
-                                               (set! str (get-output-string out))
-                                               (set-failures! t (cons str (failures t)))
-                                               (display str)))))
+          (let* ((t (current-test-runner)))
+            ((cb-test-enter t) t)
             (with-exception-handler
              (lambda (e)
-               (call-with-handle-failure print-error e))
+               ((cb-test-error t) t test-name expected 'expr e))
              (lambda ()
                (let ((res expr))
                  (cond
                   ((test? res expected)
-                   (print-case (current-output-port) (test-count t) test-name)
-                   (print-pass (current-output-port) test-name expected 'expr))
+                   ((cb-test-pass t) t test-name expected 'expr res))
                   (else
-                   (call-with-handle-failure print-fail res))))))
-            (set-test-count! t (+ (test-count t) 1))))
+                   ((cb-test-failure t) t test-name expected 'expr res))))))
+            ((cb-test-exit t) t)))
          ((_ expected expr)
           (test no-name expected expr))))))
 
@@ -190,5 +255,7 @@
   (define-syntax test-syntax-error
     (syntax-rules ()
       ((_) (syntax-error "invalid use of test-syntax-error"))))
+
+  (current-test-runner (test-runner-simple))
 
   (export test test-begin test-end test-values test-syntax-error))
