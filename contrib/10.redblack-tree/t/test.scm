634a488da422ae9f302165457fe5e96863b2c9d1
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2015-01-20 15:10:10 +0000

    add tests for rb-tree-min rb-tree-max rb-tree-delete

--- contrib/10.redblack-tree/t/test.scm
+++ contrib/10.redblack-tree/t/test.scm
@@ -1,4 +1,5 @@
 (import (scheme base)
+        (scheme write)
         (picrin rb-tree)
         (picrin test))
 
@@ -6,6 +7,7 @@
 (test (rb-tree-size tree) 0)
 (test #t (rb-tree-insert tree 1))
 (test #f (rb-tree-insert tree 1))
+
 (test (rb-tree-size tree) 1)
 (test #t (rb-tree-mem? tree 1))
 (test #f (rb-tree-mem? tree 2))
@@ -13,10 +15,16 @@
 (test #t (rb-tree-mem? tree 1))
 (test #t (rb-tree-mem? tree 2))
 
-(rb-tree-display tree) (newline)
-(rb-tree-insert tree 2)
-(rb-tree-display tree) (newline)
+(test 1 (rb-tree-min tree))
+(test 2 (rb-tree-max tree))
+
 (rb-tree-insert tree 3)
-(rb-tree-display tree) (newline)
 (rb-tree-insert tree 4)
-(rb-tree-display tree) (newline)
+(rb-tree-insert tree 5)
+
+(test 1 (rb-tree-min tree))
+(test 5 (rb-tree-max tree))
+
+(test #t (rb-tree-delete tree 1))
+(test #f (rb-tree-mem? tree 1))
+(test #f (rb-tree-delete tree 1))
