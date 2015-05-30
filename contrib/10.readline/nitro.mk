bf0e388b3ef0e8f649bb2bea93c447329871c13d
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2015-05-30 18:13:55 +0900

    support environments without libedit

--- contrib/10.readline/nitro.mk
+++ contrib/10.readline/nitro.mk
@@ -1,7 +1,10 @@
-CONTRIB_SRCS += contrib/10.readline/src/readline.c
-CONTRIB_INITS += readline
+libedit_exists := $(shell pkg-config libedit --exists; echo $$?)
 
-LDFLAGS += `pkg-config libedit --libs`
+ifeq ($(libedit_exists),0)
+  CONTRIB_SRCS += contrib/10.readline/src/readline.c
+  CONTRIB_INITS += readline
+  LDFLAGS += `pkg-config libedit --libs`
+endif
 
 contrib/src/readline.o: contrib/src/readline.c
 	$(CC) $(CFLAGS) -o $@ $< `pkg-config libedit --cflags`
