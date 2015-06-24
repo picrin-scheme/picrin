42794ebbffef6c69cf3c16cce1b982e0293e7570
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2015-06-25 07:34:10 +0900

    use khash for reader

--- extlib/benz/include/picrin/read.h
+++ extlib/benz/include/picrin/read.h
@@ -9,6 +9,8 @@
 extern "C" {
 #endif
 
+KHASH_DECLARE(read, int, pic_value)
+
 typedef pic_value (*pic_reader_t)(pic_state *, struct pic_port *port, int c);
 
 typedef struct {
@@ -16,7 +18,7 @@ typedef struct {
     PIC_CASE_DEFAULT,
     PIC_CASE_FOLD
   } typecase;
-  xhash labels;
+  khash_t(read) labels;
   pic_reader_t table[256];
   pic_reader_t dispatch[256];
 } pic_reader;
