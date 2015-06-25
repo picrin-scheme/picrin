dc71eba2966f5d51075030e8ef9aa0709c8dd00f
OGINO Masanori <masanori.ogino@gmail.com>
2015-06-25 16:42:51 +0900

    Refactor t/parameterize.scm with (picrin test).
    Signed-off-by: OGINO Masanori <masanori.ogino@gmail.com>

--- t/parameterize.scm
+++ t/parameterize.scm
@@ -1,12 +1,15 @@
 (import (scheme base)
-        (scheme write))
+        (picrin test))
 
-; expects "piece by piece by piece.\n"
-(write
- (parameterize
-     ((current-output-port (open-output-string)))
-   (display "piece")
-   (display " by piece ")
-   (display "by piece.")
-   (newline)
-   (get-output-string)))
+(test-begin)
+
+(test "piece by piece by piece.\n"
+      (parameterize
+          ((current-output-port (open-output-string)))
+        (display "piece")
+        (display " by piece ")
+        (display "by piece.")
+        (newline)
+        (get-output-string)))
+
+(test-end)
