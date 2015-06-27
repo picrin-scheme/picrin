885942b541291f05b414074a1245e225b9ed40e9
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2015-06-27 19:10:15 +0900

    more cleanup

--- extlib/benz/codegen.c
+++ extlib/benz/codegen.c
@@ -509,19 +509,13 @@ analyze_lambda(pic_state *pic, analyze_scope *scope, pic_value obj)
 }
 
 static pic_value
-analyze_declare(pic_state *pic, analyze_scope *scope, pic_sym *var)
-{
-  define_var(pic, scope, var);
-
-  return analyze_var(pic, scope, var);
-}
-
-static pic_value
 analyze_define(pic_state *pic, analyze_scope *scope, pic_value obj)
 {
   pic_value var, val;
 
-  var = analyze_declare(pic, scope, pic_sym_ptr(pic_list_ref(pic, obj, 1)));
+  define_var(pic, scope, pic_sym_ptr(pic_list_ref(pic, obj, 1)));
+
+  var = analyze(pic, scope, pic_list_ref(pic, obj, 1), false);
   val = analyze(pic, scope, pic_list_ref(pic, obj, 2), false);
 
   return pic_list3(pic, pic_obj_value(pic->sSETBANG), var, val);
