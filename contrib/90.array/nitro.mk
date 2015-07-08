370f7eb4f1657fc196ee112e6d3fd6012649769d
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2015-07-09 03:57:17 +0900

    fix array test

--- contrib/90.array/nitro.mk
+++ contrib/90.array/nitro.mk
@@ -3,4 +3,4 @@ CONTRIB_LIBS += $(wildcard contrib/90.array/*.scm)
 CONTRIB_TESTS += test-array
 
 test-array: bin/picrin
-	picrin contrib/90.array/t/array.scm
+	bin/picrin contrib/90.array/t/array.scm
