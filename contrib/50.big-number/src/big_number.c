15bdd8e9ac4e888fde3248cc02d31750e371748b
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2015-07-18 21:21:19 +0900

    Fix big_integer_add
    Support addition of numbers with different signums

--- contrib/50.big-number/src/big_number.c
+++ contrib/50.big-number/src/big_number.c
@@ -22,6 +22,136 @@ static const pic_data_type bigint_type = { "big-integer", bigint_dtor, NULL };
 
 #define BIG_NUMBER_MAX 255
 
+
+/*
+ * Eliminates all leading zeroes.
+ */
+static pic_vec *
+big_integer_vec_compact(pic_state *pic, pic_vec *v)
+{
+  int i;
+  int l = v->len - 1;
+  pic_vec *ret;
+
+  if (pic_int(v->data[l]) != 0) {
+    return v;
+  }
+  --l;
+  while (l >= 0 && pic_int(v->data[l]) == 0) {
+    --l;
+  }
+  ret = pic_make_vec(pic, l + 1);
+  for (i = 0; i <= l; ++i) {
+    ret->data[i] = v->data[i];
+  }
+
+  return ret;
+}
+
+/*
+ * Checks whether v1 and v2 represents the same value.
+ */
+static bool
+big_integer_vec_eq(pic_vec *v1, pic_vec *v2)
+{
+  int i;
+  if (v1->len != v2->len) {
+    return false;
+  }
+  for (i = v1->len - 1; i >= 0; --i) {
+    if (pic_int(v1->data[i]) != pic_int(v2->data[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+static bool
+big_integer_vec_lt(pic_vec *v1, pic_vec *v2)
+{
+  int i;
+  if (v1->len != v2->len) {
+    return v1->len < v2->len;
+  }
+  for (i = v1->len - 1; i >= 0; --i) {
+    int d1 = pic_int(v1->data[i]);
+    int d2 = pic_int(v2->data[i]);
+    if (d1 != d2) {
+      return d1 < d2;
+    }
+  }
+
+  return false;
+}
+
+static pic_vec *
+big_integer_vec_add(pic_state *pic, pic_vec *v1, pic_vec *v2)
+{
+  int carry, msb1, msb2;
+  size_t i, len;
+  pic_vec *ret;
+
+  if (v1->len == 0) {
+    return v2;
+  }
+  if (v2->len == 0) {
+    return v1;
+  }
+  // v1 > 0, v2 > 0
+  len = v1->len;
+  if (len < v2->len) {
+    len = v2->len;
+  }
+  msb1 = pic_int(v1->data[v1->len - 1]);
+  msb2 = pic_int(v2->data[v2->len - 1]);
+  if (msb1 + msb2 >= 255) {
+    ++len;
+  }
+  carry = 0;
+  ret = pic_make_vec(pic, len);
+
+  for (i = 0; i < len; ++i) {
+    int d1 = i >= v1->len ? 0 : pic_int(v1->data[i]);
+    int d2 = i >= v2->len ? 0 : pic_int(v2->data[i]);
+    carry += d1 + d2;
+    if (i == len - 1) {
+      ret->data[i] = pic_int_value(carry);
+    } else {
+      ret->data[i] = pic_int_value(carry & 0xff);
+      carry >>= 8;
+    }
+  }
+
+  return big_integer_vec_compact(pic, ret);
+}
+
+/*
+ * Precondition: v1 >= v2
+ */
+static pic_vec *
+big_integer_vec_sub(pic_state *pic, pic_vec *v1, pic_vec *v2)
+{
+  int carry;
+  size_t i, len;
+  pic_vec *ret;
+
+  len = v1->len; // v1 must be larger than v2
+  carry = 0;
+  ret = pic_make_vec(pic, len);
+
+  for (i = 0; i < len; ++i) {
+    int d1 = pic_int(v1->data[i]);
+    int d2 = i >= v2->len ? 0 : pic_int(v2->data[i]);
+    carry += d1 - d2;
+    ret->data[i] = pic_int_value(carry & 0xff);
+    carry >>= 8;
+  }
+
+  return big_integer_vec_compact(pic, ret);
+}
+
+
 /*
  * Creates a big integer by the given int value.
  */
@@ -41,7 +171,7 @@ big_integer_init_int(pic_state *pic, int value)
   for (i = 0; i < 4; ++i) {
     bn->data[i] = pic_int_value((value >> (8 * i)) & 0xff);
   }
-  bi->digits = bn;
+  bi->digits = big_integer_vec_compact(pic, bn);
 
   return bi;
 }
@@ -49,36 +179,27 @@ big_integer_init_int(pic_state *pic, int value)
 static struct pic_bigint_t *
 big_integer_add(pic_state *pic, struct pic_bigint_t *bn1, struct pic_bigint_t *bn2)
 {
-  size_t i, len;
-  int msb1, msb2, carry;
-  pic_vec *ret;
   struct pic_bigint_t *retbi;
 
-  len = bn1->digits->len;
-  if (len < bn2->digits->len) {
-    len = bn2->digits->len;
-  }
-  msb1 = pic_int(bn1->digits->data[bn1->digits->len - 1]);
-  msb2 = pic_int(bn2->digits->data[bn2->digits->len - 1]);
-  if (msb1 + msb2 >= 255 || msb1 + msb2 <= -256) {
-    ++len;
-  }
-  carry = 0;
-  ret = pic_make_vec(pic, len);
   retbi = pic_malloc(pic, sizeof(struct pic_bigint_t));
-  retbi->digits = ret;
 
-  for (i = 0; i < len; ++i) {
-    int d1 = i >= bn1->digits->len ? 0 : pic_int(bn1->digits->data[i]);
-    int d2 = i >= bn2->digits->len ? 0 : pic_int(bn2->digits->data[i]);
-    carry += d1 + d2;
-    if (i == len - 1) {
-      ret->data[i] = pic_int_value(carry);
-    } else {
-      ret->data[i] = pic_int_value(carry & 0xff);
-      carry >>= 8;
+  if (bn1->signum != bn2->signum) {
+    if (big_integer_vec_lt(bn1->digits, bn2->digits)) { // bn2 wins
+      retbi->signum = bn2->signum;
+      retbi->digits = big_integer_vec_sub(pic, bn2->digits, bn1->digits);
+      return retbi;
     }
+    retbi->signum = bn1->signum;
+    retbi->digits = big_integer_vec_sub(pic, bn1->digits, bn2->digits);
+    if (retbi->digits->len == 0) { // bn1 + bn2 == 0
+      retbi->signum = 0;
+    }
+    return retbi;
   }
+  // two signums are equal
+  retbi->signum = bn1->signum;
+  retbi->digits = big_integer_vec_add(pic, bn1->digits, bn2->digits);
+
   return retbi;
 }
 
