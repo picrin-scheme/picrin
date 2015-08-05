43aaebd37ff1d74aec4dd340e0ef223b123837eb
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2015-08-05 12:46:04 +0900

    Rewrite tests for big-number

--- contrib/50.big-number/t/test.scm
+++ contrib/50.big-number/t/test.scm
@@ -3,73 +3,26 @@
         (picrin big-number)
         (srfi 27))
 
-(define (test-add x y)
-  (let ((a (make-bigint x)) (b (make-bigint y)))
-    (test
-      (bigint-underlying (make-bigint (+ x y)))
-      (bigint-underlying (bigint-add a b)))))
 
-(define (test-sub x y)
-  (let ((a (make-bigint x)) (b (make-bigint y)))
-    (test
-      (bigint-underlying (make-bigint (- x y)))
-      (bigint-underlying (bigint-sub a b)))))
-
-(define (test-mul x y)
-  (let ((a (make-bigint x)) (b (make-bigint y)))
-    (test
-      (bigint-underlying (make-bigint (* x y)))
-      (bigint-underlying (bigint-mul a b)))))
-
-(define (bi-add x y)
-  (bigint-underlying
-    (bigint-add
-      (make-bigint x)
-      (make-bigint y))))
-
-(define (test-equal x y)
-  (let ((a (make-bigint x)) (b (make-bigint y)))
-    (test
-        (equal? x y)
-        (bigint-equal? a b))))
-
-(define (test-bigint->number x y)
-  (let ((a (make-bigint x)) (b (make-bigint y)))
-    (test (+ x y)
-      (bigint->number (bigint-add a b)))))
-
-
-(define (rand-int n) (floor (* n (random-real))))
-(define (test-random func time)
-  (do ((iteration 0 (+ iteration 1)))
-    ((>= iteration time) #t)
-    (let ((x (rand-int 32535)) (y (rand-int 32535)))
-      (func x y))))
-
-(test-random test-add 3)
-(test-random test-equal 3)
-(test-random test-bigint->number 3)
-(test-random test-mul 3)
-
-
+; ->number
 (test 192.0 (bigint->number (bigint-asl (make-bigint "12") 4)))
 (test 384.0 (bigint->number (bigint-asl (make-bigint "3") 7)))
 
-
+; underlying
 (test #(2582) (bigint-underlying (make-bigint "2582")))
-(test #f (bigint-less? (make-bigint "12") (make-bigint "11")))
-(test #t (bigint-less? (make-bigint "-12") (make-bigint "-11")))
-(test #f (bigint-less? (make-bigint "-0") (make-bigint "0")))
 
 
 ; addition
-(test #t
-  (bigint-equal? (make-bigint "2600363440")
-    (bigint-add (make-bigint "215127315") (make-bigint "2385236125"))))
-(test (bigint-underlying (make-bigint "6902703869434847155238669212227261023932199886824539729901626742813527050006482753964415641898340566"))
-    (bigint-underlying (bigint-add (make-bigint "6749346968279885792917645501676082665015680331686594141390629938583418409197715730606872803686797073")
-      (make-bigint "153356901154961362321023710551178358916519555137945588510996804230108640808767023357542838211543493"))))
-
+(define-syntax test-add
+  (syntax-rules ()
+    ((_ a b ab)
+      (test #t (bigint-equal? (make-bigint ab) (bigint-add (make-bigint a) (make-bigint b)))))))
+(test-add "215127315" "2385236125" "2600363440")
+(test-add
+  "6749346968279885792917645501676082665015680331686594141390629938583418409197715730606872803686797073"
+   "153356901154961362321023710551178358916519555137945588510996804230108640808767023357542838211543493"
+  "6902703869434847155238669212227261023932199886824539729901626742813527050006482753964415641898340566")
+(test-add "4" "-2" "2")
 
 ; multiplication
 
@@ -84,6 +37,28 @@
   (bigint-equal? (make-bigint "18446744065119617025")
     (let ((ff32 (make-bigint "4294967295")))
     (bigint-mul ff32 ff32))))
+
+; equality
+(define-syntax test-equal?
+  (syntax-rules ()
+    ((_ a b)
+      (test (equal? a b)
+        (bigint-equal? (make-bigint a) (make-bigint b))))))
+
+(test-equal? "12345" "12345")
+(test-equal? "1" "4294967297")
+
+; less
+(define-syntax test-less?
+  (syntax-rules ()
+    ((_ a b ab)
+      (test ab
+        (bigint-less? (make-bigint a) (make-bigint b))))))
+(test-less? "12" "11" #f)
+(test-less? "-12" "-11" #t)
+(test-less? "-0" "0" #f)
+(test-less? "1" "4294967297" #t)
+
 
 ; factorial
 
