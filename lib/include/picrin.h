ad1912119d9836f074291c2269be9f2ab847674a
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2017-03-23 01:27:05 +0900

    update copyright year

--- lib/include/picrin.h
+++ lib/include/picrin.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013-2016 Picrin developers.
+ * Copyright (c) 2013-2017 Picrin developers.
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
